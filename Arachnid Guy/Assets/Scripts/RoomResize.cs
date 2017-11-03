4588d71874fd59a6a32f500d35d04f6ce068a4ca
Dat Nguyen <dvnguyen95@gmail.com>
2017-11-03 03:47:20 -0500

    Updated hub0.1 scene to be a dynamically sized room. Re-imported vital SteamVR assets, excluding extraneous files.

---
+++ Arachnid Guy/Assets/Scripts/RoomResize.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class RoomResize : MonoBehaviour {
+	public Vector3 size;
+	private GameObject northWall;
+	private GameObject eastWall;
+	private GameObject southWall;
+	private GameObject westWall;
+	private GameObject floor;
+
+	void Awake () {
+		northWall = GameObject.Find ("NorthWall");
+		eastWall = GameObject.Find ("EastWall");
+		southWall = GameObject.Find ("SouthWall");
+		westWall = GameObject.Find ("WestWall");
+		floor = GameObject.Find ("Floor");
+	}
+	// Use this for initialization
+	void Start () {
+		//this method of finding room size courtesy of user Lukeus_Maximus in a reddit post 
+		HmdQuad_t hmdQuad = new HmdQuad_t();
+		if (SteamVR_PlayArea.GetBounds (SteamVR_PlayArea.Size.Calibrated, ref hmdQuad)) {
+			float width = Mathf.Abs (hmdQuad.vCorners0.v0 - hmdQuad.vCorners1.v0);
+			float length = Mathf.Abs (hmdQuad.vCorners2.v2 - hmdQuad.vCorners1.v2);
+			size = new Vector3 (width, 0, length);
+		} else {
+			return;
+		}
+
+		northWall.transform.position = new Vector3 (size.x/2,0,0);
+		southWall.transform.position = new Vector3 (-size.x/2,0,0);
+		eastWall.transform.position = new Vector3 (0,0,-size.z/2);
+		westWall.transform.position = new Vector3 (0,0,size.z/2);
+		floor.transform.localScale = new Vector3 (size.x/2,0,size.z/2);
+
+	}
+
+}
